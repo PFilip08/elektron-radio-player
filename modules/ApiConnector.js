49c887684a43d798cddffc03c054ed9264f1ce5e
Patryk <43276401+Zaptyp@users.noreply.github.com>
2024-06-08 14:51:37 +0200

    Add more logs and fix some issues (#1)
    * Update dependency
    
    * Translate logs in MusicDownloader
    
    * Translate logs and add info about turning scripts with isOn true in ApiConnector
    
    * Translate logs and fix one mistake in MusicPlayer
    
    * Translate logs and change startup order in POST.js
    
    * Translate logs in TaskScheduler

--- modules/ApiConnector.js
+++ modules/ApiConnector.js
@@ -5,6 +5,7 @@ import {logger} from "./Logger.js";
 let url = 'https://radio-elektron.vercel.app/api/timeTables';
 let previousData = null;
 let messageCounter = false;
+let messageStartupBlocker = false;
 
 const api = axios.create({
     httpsAgent: new Agent({
@@ -24,9 +25,17 @@ const api = axios.create({
 async function getApiData() {
     return await api.get(url)
         .then(res => {
+            if (!messageCounter) {
+                if (res.data.timeTable[0].isOn === false) {
+                    if (!messageStartupBlocker) {
+                        messageStartupBlocker = true;
+                        logger('warn','Włączono z blokadą!!!','getApiData');
+                    }
+                }
+            }
             if (messageCounter) {
-                logger('log','Restored Internet connection','getApiData');
-                logger('log','Using API data','getApiData');
+                logger('log','Przywrócono połączenie z internetem','getApiData');
+                logger('log','Używanie danych z API','getApiData');
                 messageCounter = false;
             }
             if (res.data.timeTable[0].isOn === false) {
@@ -36,12 +45,16 @@ async function getApiData() {
         })
         .catch(()=>{
             // console.log(err);
-            if (!messageCounter) logger('log','Recovery mode activated!!!','getApiData');
+            if (!messageCounter) logger('warn','Tryb RECOVERY AKTYWNY!!!','getApiData');
             // let res =
-            let res = {"static":true,"isOn":true,"currentPlaylistId":1,"timeRules":{"rules":{"1":[{"end":"07:10","start":"07:07"},{"end":"08:00","start":"07:55"},{"end":"08:50","start":"08:45"},{"end":"09:45","start":"09:35"},{"end":"10:35","start":"10:30"},{"end":"11:40","start":"11:20"},{"end":"12:30","start":"12:25"},{"end":"13:20","start":"13:15"},{"end":"14:10","start":"14:05"},{"end":"14:55","start":"15:00"},{"end":"15:50","start":"15:45"}]},"applyRule":{"Fri":1,"Mon":1,"Sat":0,"Sun":0,"Thu":1,"Tue":1,"Wed":1}}};
+            let res = {"static":true,"isOn":true,"currentPlaylistId":1,"timeRules":{"rules":{"1":[{"end":"07:10","start":"07:07"},{"end":"08:00","start":"07:55"},{"end":"08:50","start":"08:45"},{"end":"09:45","start":"09:35"},{"end":"10:35","start":"10:30"},{"end":"11:40","start":"11:20"},{"end":"12:30","start":"12:25"},{"end":"13:20","start":"13:15"},{"end":"14:10","start":"14:05"},{"end":"15:00","start":"14:55"},{"end":"15:50","start":"15:45"}]},"applyRule":{"Fri":1,"Mon":1,"Sat":0,"Sun":0,"Thu":1,"Tue":1,"Wed":1}}};
             if (previousData !== null) {
                 res = previousData;
-                if (!messageCounter) logger('warn','Using cached data.','getApiData');
+                if (!messageCounter) logger('warn','Używanie danych pobranych poprzednio z API!','getApiData');
+            }
+            if (res.static) {
+                messageStartupBlocker = true;
+                if (!messageCounter) logger('warn','Używanie danych statycznych!.','getApiData');
             }
             if (!messageCounter) messageCounter = true;
             return res;
@@ -50,26 +63,25 @@ async function getApiData() {
 
 async function checkUpdate() {
     try {
+        messageStartupBlocker = true;
         const currentData = await getApiData();
-
         if (currentData.static) {
-            if (!messageCounter) logger('warn','Using static data.','getApiData');
             return;
         }
 
         if (previousData && JSON.stringify(currentData) !== JSON.stringify(previousData)) {
             massSchedule();
             logger('log', '--------Check Update--------','checkUpdate');
-            logger('log', 'Updated','checkUpdate');
+            logger('log', 'Pobrano dane z API','checkUpdate');
             logger('log', '--------Check Update--------','checkUpdate');
         }
 
         previousData = currentData;
     } catch (error) {
-        logger('log', '--------Check Update--------','checkUpdate');
-        logger('log', `Błąd podczas odpytywania API: ${error}`,'checkUpdate');
-        logger('log', '--------Check Update--------','checkUpdate');
+        logger('error', '--------Check Update--------','checkUpdate');
+        logger('error', `Błąd podczas odpytywania API: ${error}`,'checkUpdate');
+        logger('error', '--------Check Update--------','checkUpdate');
     }
 }
 
-export {getApiData, checkUpdate};
+export { getApiData, checkUpdate };
