890fa71c9ab9046b309cf7425dd6f672a7dd3a38
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2024-06-11 07:16:13 +0200

    Uncoment win32 block

--- modules/POST.js
+++ modules/POST.js
@@ -7,12 +7,10 @@ async function POST() {
     logger('POST', '    elektron-radio-player')
     logger('POST', '        By PFilip :>')
     logger('POST', '------------------------------')
-    /*
     if (process.platform === "win32") {
         logger('error', 'okna niedozwolone');
         return process.exit(2);
     }
-    */
     logger('task', 'Planowanie zadań…', 'POST');
     await massSchedule();
     logger('task', 'Aktywowanie automatycznych aktualizacji z API', 'POST');
