b0e769969ee7ef9514e1171335db7157f545e056
PFilip <2008filip@wp.pl>
2024-08-14 22:18:26 +0200

    fix fix

--- api/public/music.js
+++ api/public/music.js
@@ -86,6 +86,10 @@ async function replaceData() {
         const row = songsTable.insertRow(-1);
         row.insertCell(0).innerText = 'brak danych';
         row.insertCell(1).innerText = 'brak danych';
+        if (oldRowId) {
+            playlistsTable.rows[oldRowId].style.removeProperty('color');
+            playlistsTable.rows[oldRowId].style.removeProperty('background-color');
+        }
         currentSongData=null;
         return;
     }
