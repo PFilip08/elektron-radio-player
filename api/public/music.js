21865b3c9aa70e614bf54e3c84d6fe532a405a82
PFilip <2008filip@wp.pl>
2024-08-14 22:02:00 +0200

    zmiana sterylizatora

--- api/public/music.js
+++ api/public/music.js
@@ -1,5 +1,8 @@
 function kastracja(input) {
-    return input.split(' ').join('_').replace(/[^a-zA-Z_0-9-\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/g, "");
+    return input
+        .toLowerCase()  // Konwertuje cały tekst na małe litery
+        .split(' ').join('_')  // Zmienia spacje na podkreślenia
+        .replace(/[^a-z0-9_\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/g, "");  // Usuwa niepożądane znaki
 }
 
 async function getData() {
