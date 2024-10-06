cf6812abb7ec2f1690981f82d8d8a1018305a6e5
PFilip <2008filip@wp.pl>
2024-10-06 12:59:25 +0200

    hotfix, bo axios i insecureHTTPParser

--- api/controllers/power.controller.js
+++ api/controllers/power.controller.js
@@ -3,21 +3,30 @@
 import {logger} from "../../modules/Logger.js";
 import {DebugSaveToFile} from "../../modules/DebugMode.js";
 import axios from "axios";
+import * as http from "node:http";
 
 const wzmakURI = `http://${process.env.WZMAK}/cm?cmnd=`;
 const mixerURI = `http://${process.env.MIXER}/cm?cmnd=`;
+
+const api = axios.create({
+    httpAgent: new http.Agent(),
+    insecureHTTPParser: true,
+    headers: {
+        'User-Agent': 'Radio-Elektron_SAW_MONITORING v1.0'
+    }
+});
 
 export async function wzmakPower(req, res) {
     try {
         const action = req.query.action;
         if (action === 'on') {
             logger('log', `Otrzymano request od ${req.hostname} ${req.get('User-Agent')}!`, 'LocalAPI - wzmakPower');
-            return res.status(200).send((await (await axios.get(wzmakURI + 'POWER ON')).data));
+            return res.status(200).send((await (await api.get(wzmakURI + 'POWER ON')).data));
         } else if (action === 'off') {
             logger('log', `Otrzymano request od ${req.hostname} ${req.get('User-Agent')}!`, 'LocalAPI - wzmakPower');
-            return res.status(200).send((await (await axios.get(wzmakURI + 'POWER OFF')).data));
+            return res.status(200).send((await (await api.get(wzmakURI + 'POWER OFF')).data));
         } else if (action === 'status') {
-            return res.status(200).send((await (await axios.get(wzmakURI + 'status 8')).data.StatusSNS.ENERGY));
+            return res.status(200).send((await (await api.get(wzmakURI + 'status 8')).data.StatusSNS.ENERGY));
         }
         return res.status(402).send('nieznanen');
     } catch (e) {
@@ -26,6 +35,7 @@ export async function wzmakPower(req, res) {
             DebugSaveToFile('LocalAPI', 'wzmakPower', 'catched_error', e);
             logger('verbose', `Stacktrace został zrzucony do debug/`, 'LocalAPI - wzmakPower');
         }
+        return res.status(500).send('Błąd komunikacji z wtyczką');
     }
 }
 
@@ -34,12 +44,12 @@ export async function mixerPower(req, res) {
         const action = req.query.action;
         if (action === 'on') {
             logger('log', `Otrzymano request od ${req.hostname} ${req.get('User-Agent')}!`, 'LocalAPI - mixerPower');
-            return res.status(200).send((await (await axios.get(mixerURI + 'POWER ON')).data));
+            return res.status(200).send((await (await api.get(mixerURI + 'POWER ON')).data));
         } else if (action === 'off') {
             logger('log', `Otrzymano request od ${req.hostname} ${req.get('User-Agent')}!`, 'LocalAPI - mixerPower');
-            return res.status(200).send((await (await axios.get(mixerURI + 'POWER OFF')).data));
+            return res.status(200).send((await (await api.get(mixerURI + 'POWER OFF')).data));
         } else if (action === 'status') {
-            return res.status(200).send((await (await axios.get(mixerURI + 'status 8')).data.StatusSNS.ENERGY));
+            return res.status(200).send((await (await api.get(mixerURI + 'status 8')).data.StatusSNS.ENERGY));
         }
         return res.status(402).send('nieznanen');
     } catch (e) {
@@ -48,5 +58,6 @@ export async function mixerPower(req, res) {
             DebugSaveToFile('LocalAPI', 'mixerPower', 'catched_error', e);
             logger('verbose', `Stacktrace został zrzucony do debug/`, 'LocalAPI - mixerPower');
         }
+        return res.status(500).send('Błąd komunikacji z wtyczką');
     }
 }
