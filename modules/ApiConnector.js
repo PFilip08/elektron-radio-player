0467ef4813632f1d858fc2d5ab8ab94eb198cf24
PFilip <2008filip@wp.pl>
2024-11-12 20:14:58 +0100

    code cleanup v2

--- modules/ApiConnector.js
+++ modules/ApiConnector.js
@@ -20,12 +20,6 @@ const api = axios.create({
     }
 });
 
-// async function getApiData() {
-//     api.get(url)
-//         .then(res => console.log(res.data.timeTable[0].timeRules.rules))
-//         .catch(err=>console.log(err));
-// }
-
 async function getApiData() {
     return await api.get(url)
         .then(res => {
@@ -57,7 +51,6 @@ async function getApiData() {
             return res.data.timeTable[0];
         })
         .catch((error)=>{
-            // console.log(err);
             logger('verbose','Wchodzenie w tryb recovery...','getApiData');
             logger('verbose',`Złapano błąd: ${error}`,'getApiData');
             if (global.debugmode === true) {
@@ -65,7 +58,6 @@ async function getApiData() {
                 logger('verbose',`Pełen stackrace zrzucono do pliku recovery_reason.json!`,'getApiData');
             }
             if (!messageCounter) logger('warn','Tryb RECOVERY AKTYWNY!!!','getApiData');
-            // let res =
             let res = {"static":true,"isOn":true,"currentPlaylistId":1,"timeRules":{"rules":{"1":[{"end":"07:10","start":"07:07"},{"end":"08:00","start":"07:55"},{"end":"08:50","start":"08:45"},{"end":"09:45","start":"09:35"},{"end":"10:35","start":"10:30"},{"end":"11:40","start":"11:20"},{"end":"12:30","start":"12:25"},{"end":"13:20","start":"13:15"},{"end":"14:10","start":"14:05"},{"end":"15:00","start":"14:55"},{"end":"15:50","start":"15:45"}]},"applyRule":{"Fri":1,"Mon":1,"Sat":0,"Sun":0,"Thu":1,"Tue":1,"Wed":1}}};
             if (previousData !== null) {
                 res = previousData;
