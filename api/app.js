e1399f8ecccd0ccc8c366b17db9c23b9888e68bf
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2025-02-17 23:21:30 +0100

    Add EKonfident module

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ import {fileURLToPath} from 'url';
 import * as os from "node:os";
 import {DebugSaveToFile} from '../modules/DebugMode.js';
 import {previousData} from "../modules/ApiConnector.js";
+import colors from 'colors';
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -56,7 +57,43 @@ app.get('/stats/api', function(req, res){
 app.get('/stats/data', async function (req, res) {
   res.status(200).send(previousData);
 });
-
+app.post('/stats/confident', async function (req, res) {
+  const logType = req.query.type;
+  switch (logType) {
+    case 'safeguardd':
+      logger('warn', `UWAGA! WYŁĄCZONO SAFEGUARD!!! EKonfident nie będzie rejestrował prób puszczenia muzyki!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'safeguarde':
+      logger('log', `UWAGA! WŁĄCZONO SAFEGUARD!!! EKonfident będzie ponownie rejestrował próby puszczenia muzyki!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'weekend':
+      logger('warn', `UWAGA! Puszczono muzykę w WEEKEND!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'weekenda':
+      logger('log', `UWAGA! Próba puszczenia muzyki w WEEKEND!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'lesson':
+      logger('warn', `UWAGA! Puszczono muzykę w trakcie LEKCJI!!! MOŻLIWE SĄ POWAŻNE KONSEKWENCJĘ DLA RADIA!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'lessona':
+      logger('log', `UWAGA! Próba puszczenia muzyki w trakcie LEKCJI!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'hebel':
+      logger('warn', `UWAGA! Próba puszczenie muzyki gdy zarządzono CAŁKOWITY ZAKAZ PUSZCZANIA MUZYKI!!! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'after':
+      logger('warn', `UWAGA! Próba puszczenia muzyki po godzinach pracy radia! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`, 'LocalAPI - EKonfident');
+      break;
+    case 'logicfail':
+      logger('error', colors.yellow(`UWAGA! Logika safeguarda się posypała! Request od ${req.hostname} o useragencie ${req.get('User-Agent')}`), 'LocalAPI - EKonfident');
+      break;
+    default:
+      res.status(400).send('Nieznany typ logu');
+      break;
+  }
+  logger('verbose', 'PS: Aby wyłączyć lub włączyć EKonfidenta, wpisz komendę "toggleSafeguard()" w konsoli przeglądarki', 'LocalAPI - EKonfident');
+  res.status(200).send('gut confident');
+});
 app.use('/download', downloadRouter);
 app.use('/action', actionsRouter);
 app.use('/status', statusRouter);
