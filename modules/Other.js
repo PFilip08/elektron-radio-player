7bbcb907fb204eda1969d56795e05284810d4a60
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2026-02-21 11:42:59 +0100

    Update pathSecurityChecker to include checks for any folder

--- modules/Other.js
+++ modules/Other.js
@@ -55,7 +55,7 @@ function sterylizator(input) {
     return sterilised;
 }
 
-function pathSecurityChecker(filepath) {
+function pathSecurityChecker(filepath, defendingFolder = 'mp3') {
     // Sprawdza czy ścieżka nie może wykonać Path Travels
     logger('verbose', `Sprawdzanie bezpieczeństwa ścieżki: ${filepath}`, 'pathSecurityChecker');
     if (global.debugmode === true) {
@@ -68,15 +68,15 @@ function pathSecurityChecker(filepath) {
         return 'NULL_BYTE_ATTEMPT';
     }
     logger('verbose', 'Sprawdzanie czy ścieżka nie wiedzie do ucieczki z głównego folderu', 'pathSecurityChecker');
-    const rootDirectory = path.resolve(process.cwd(), 'mp3');
+    const rootDirectory = path.resolve(process.cwd(), defendingFolder);
     const filename = path.join(rootDirectory, filepath);
     if (filename.indexOf(rootDirectory) !== 0) {
-        logger('warn', yellow(`Próba wyjścia poza katalog główny mp3!!!`), 'pathSecurityChecker');
+        logger('warn', yellow(`Próba wyjścia poza katalog ${defendingFolder}!!!`), 'pathSecurityChecker');
         return 'ROOT_EXIT_ATTEMPT';
     }
     logger('verbose', 'Sprawdzanie czy ścieżka nie zawiera dwukropków', 'pathSecurityChecker');
     if (!/^[a-zA-Z0-9/._-]+$/.test(filepath)) {
-        logger('warn', yellow(`Próba wyjścia poza katalog główny przy użyciu dwukropków!!!`), 'pathSecurityChecker');
+        logger('warn', yellow(`Próba wyjścia poza katalog ${defendingFolder} przy użyciu dwukropków!!!`), 'pathSecurityChecker');
         return 'ESCAPE_PATH_ATTEMPT';
     }
     return 'NONE'
