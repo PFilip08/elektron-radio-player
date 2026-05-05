bbc6573a82e8ca2cca36316fd413260aa8328fe9
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2026-05-05 09:05:18 +0200

    Move performAction from admin.js

--- api/public/js/admin.js
+++ api/public/js/admin.js
@@ -1,20 +1,3 @@
-function performAction(url, params = {}) {
-    const queryString = new URLSearchParams(params).toString();
-    const iframe = document.getElementById('res');
-    fetch(`${url}?${queryString}`, { method: 'POST' })
-        .then(response => response.text())
-        .then(data => {
-            console.log('Akcja wykonana:', data);
-            iframe.contentWindow.document.open();
-            iframe.contentWindow.document.write(data);
-            iframe.contentWindow.document.close();
-        })
-        .catch(error => {
-            console.error('Błąd:', error);
-            alert('Wystąpił błąd podczas wykonywania akcji.\nSkontaktuj się z Działem Taboretów.');
-        });
-}
-
 const ekonfidentUri = '/security/confident';
 
 function toggleSafeguard() {
